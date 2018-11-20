16812aa3cf94cb33565cd2eebfcce275c859e8e6
Alby Bott <albybott@gmail.com>
2018-11-20 16:50:18 +1300

    Add Typography margin bottom default

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,23 +1,23 @@
 import Typography from 'typography'
-import funstonTheme from 'typography-theme-funston'
+// import funstonTheme from 'typography-theme-funston'
 
 const typography = new Typography({
   // this is a typography.js theme
-  // import with npm and add as per below
-  funstonTheme,
-  includeNormalize: false,
+  // funstonTheme,
 
-  // baseFontSize: '18px',
-  // baseLineHeight: 1.666,
-  // headerFontFamily: [
-  //   'Avenir Next',
-  //   'Helvetica Neue',
-  //   'Segoe UI',
-  //   'Helvetica',
-  //   'Arial',
-  //   'sans-serif',
-  // ],
-  // bodyFontFamily: ['Georgia', 'serif'],
+  includeNormalize: false, // Dont add normalize.css (added in the GlobalStyle)
+  blockMarginBottom: 0, // Default margin bottom for all block elements
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    'Avenir Next',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  bodyFontFamily: ['Georgia', 'serif'],
 })
 
 export default typography
