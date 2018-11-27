0b50b5f8f34fa373c220c06007725d648033fe93
Alby Bott <albybott@gmail.com>
2018-11-27 21:24:42 +1300

    Add whaikorero

--- data.js
+++ data.js
@@ -196,5 +196,71 @@ module.exports = {
         },
       ],
     },
+    {
+      heading: {
+        title: 'HE MIHI: MANUHIRI',
+        translation: 'A SPEECH AS A VISITOR',
+      },
+      group: 'Whaikorero',
+      lines: [
+        {
+          maori: 'He honore, he korōria ki te Atua	',
+          eng: 'Honour and glory to the Creator',
+        },
+        { maori: 'He maungarongo ki te whenua', eng: 'Peace on earth' },
+        {
+          maori: 'He whakaaro pai ki ngā tāngata katoa',
+          eng: 'Goodwill to all mankind',
+        },
+        {
+          maori: 'Tihei Mauri Ora,',
+          eng: 'I breath and it is life',
+        },
+        {
+          maori: 'E te whare e tū nei, tēnā koe,',
+          eng: 'I acknowledge the house standing here',
+        },
+        {
+          maori: 'E te marae e hora nei, tēnā koe, tēnā korua,',
+          eng: 'I acknowledge the marae residence',
+        },
+        {
+          maori: 'E tautoko ana au ngā mihi ki te kīngi Māori',
+          eng: 'I support the acknowledgement to the',
+        },
+        {
+          maori: 'ki a Tūheitia, Pai Marire,',
+          eng: 'Maori King, Tuheitia',
+        },
+        {
+          maori: 'Āpiti hono, tātai hono, tātou te hunga ora ki a tātou',
+          eng: 'The living to the living',
+        },
+        {
+          maori: 'E ngā mate, haere, haere, haere atu rā',
+          eng: 'To those who have passed on, farewell',
+        },
+        {
+          maori: 'Haere ki te pō nui, te pō roa, te pō tangotango',
+          eng: 'return to the great, vast and endless night',
+        },
+        {
+          maori: 'Āpiti hono, tātai hono, rātou te hunga mate ki a rātou',
+          eng: 'The dead to the dead',
+        },
+        {
+          maori: 'E te hau kāinga, karanga mai, karanga mai, mihi mai',
+          eng: 'to the host, welcome us your visitors',
+        },
+        {
+          maori: 'Kei te harikoa tōku ngākau kua tae mai mātou',
+          eng: 'My heart is pleased that we have arrived',
+        },
+        {
+          maori: 'Nā reira, tēnā koutou, tēnā koutou, tēnā koutou katoa',
+          eng: 'Therefore I acknowledge you all',
+        },
+      ],
+    },
   ],
 }
